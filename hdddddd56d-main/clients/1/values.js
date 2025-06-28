0b9e43c9b5a9964fbc346bb8d086dbd841668516
papa-docs <papa-docs1@proton.me>
2025-06-28 19:56:32 +0300

    Add files via upload

---
+++ hdddddd56d-main/clients/1/values.js
@@ -0,0 +1,56 @@
+// values.js
+
+// данные 
+var fio               = "Буденко Георгiй Вячеславич";
+var fio_en            = "Budenko Heorgii Vyacheslavich";
+var birth             = "18.05.1989";
+var rnokpp            = "3264518653";
+
+var prava_number      = "AB734295"; // номер прав
+var university        = "ХНУ имени Каразина"; // Університет
+var fakultet          = "Физико-технический"; // Факультет
+var stepen_dip        = "Магістра";
+var univer_dip        = "ХНУ имени Каразина";
+var dayout_dip        = "01.07.2023";
+var special_dip       = "Прикладная математика";
+var number_dip        = "MT-545678";
+var live              = "ХАРКIВСЬКА ОБЛ.";
+var prava_date_out    = "09.11.2062";
+var bank_adress       = "м. Шостка, Луговая 9";
+var sex               = "Ч";
+var sex_en            = "M";
+
+var zagran_number     = "FX013559";
+var student_number    = "2022154258";
+var student_date_give = "01.09.2021";
+var student_date_out  = "30.06.2025";
+var form              = "Очная";
+var rights_categories = "A, B";
+var date_give         = "22.11.2018";
+var prava_date_give   = "22.11.2019";
+var date_out          = "09.11.2062";
+var pass_number       = "008759371";
+var organ             = "5928";
+var uznr              = "19890518-08650";
+var legalAdress       = "УКРАЇНА СУМСЬКА ОБЛАСТЬ ШОСТКИНСЬКИЙ РАЙОН М. ШОСТКА вул. ЛУГОВА БУД. 9";
+var registeredOn      = "15.07.2007";
+var pravaOrgan        = "ТСЦ 6345 МВС України";
+
+
+// Включение/выключение документов
+var isRightsEnabled   = true;
+var isStudyEnabled    = false;
+var isZagranEnabled   = true;
+var isDiplomaEnabled  = false;
+// Добавь остальные переменные по аналогии
+
+// Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
+
+var photo_passport = "clients/1/1.png"; //эДок, паспорт,
+var photo_rights   = "clients/1/2.png"; //права
+var photo_students = "clients/1/1.png"; //диплом, студ
+var photo_zagran   = "clients/1/1.png"; //загран
+
+var signPng           = "clients/1/sign.png"; //подпись
+// Можно добавить еще, если надо
+
